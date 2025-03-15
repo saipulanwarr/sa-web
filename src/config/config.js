dc6df62f5ee826294aeb6bf2fd2f9a926128a9d4
saipulanwarr <saipulanwar180994@gmail.com>
2025-03-15 22:32:44 +0700

    bugfix

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://saweb.saipulanwar.my.id/api";
-const IMAGE_BASE_URL = "http://saweb.saipulanwar.my.id";
+const API_BASE_URL = "https://saweb.saipulanwar.my.id/api";
+const IMAGE_BASE_URL = "https://saweb.saipulanwar.my.id";
 
 export { API_BASE_URL, IMAGE_BASE_URL };
